da7b1f63cd79be0e51e2271a584bdba0aa031697
HyoBeom <hyobeom@codemind.co.kr>
2021-09-27 17:10:13 +0900

    cron job에서 interval로 변경

--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -1,5 +1,5 @@
 import { Body, Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression, Interval } from '@nestjs/schedule';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { log } from 'console';
 import * as request from 'request';
@@ -8,21 +8,26 @@ import { GameID } from './entities/gameId.entity';
 import { Route } from './entities/route.entity';
 
 const BATTLE_TYPE_SOLO = 1; 
-const DATA_EXIST = 1; 
-const DATA_NOT_EXIST = 2; 
-const DATA_TOO_MANY_REQUEST = 3; 
+
+class InstanceData{
+    gameId = 0;
+    gameJson;
+    routeId = 0;
+    routeJson;
+    rank_1st_Json;
+    inGame_Character_list: number[];
+    too_Many_Request = false;
+    constructor(gameId){
+        this.gameId = gameId;
+    }
+};
+
 
 @Injectable()
 export class WorkerService {
 
   private static errorCount = 0;
-  private static gameId = 14;
-  private static gameJson;
-  private static routeId = 0;
-  private static routeJson;
-  private static rank_1st_Json;
-  private static inGame_Character_list: number[];
-
+  private static gameId = 0;
 
   constructor(
     @InjectRepository(Route)
@@ -30,27 +35,33 @@ export class WorkerService {
     @InjectRepository(GameID)
     private gmaeIdRepository: Repository<GameID>,
   ) {
-
   }
 
-  @Cron(CronExpression.EVERY_SECOND)
+
+  @Interval(100)
   async analys() {
-        
-        if(!this.check_Error_Count()) return //api가 n번 연속이면 다시 gameId - n 부터 다시 조회 시작. ( n = ERROR_TIME_NUM)
-        
         WorkerService.gameId = await this.get_game_ID();
-        
+        const instanceData = new InstanceData(WorkerService.gameId);
+
+        if(!this.check_Error_Count()) return // error 횟수가 연속 n번 이상이면 n번 이전 부터 다시 분석
         await this.set_game_ID(WorkerService.gameId +1);
-
-        if(!await this.check_Data_Exist()) return //데이터 체크 및 에러 횟수 카운트 -> 5번연속이면 다시 5번째로 다시 돌아가고 다시 0 -> 휴면기 돌입 1시간.
         
-        if(!await this.check_BattleType_Solo()) return // battleType === BATTLETYPE_SOLO 체크
-        
-        if(!await this.check_Exist_RouteId()) return //  routeId 체크
 
-        if(!await this.check_Route_Data()) return
+        // too Many requests면 다시 분석한다.
+        do{
+            
+            if(!await this.check_Data_Exist(instanceData)) continue  //데이터 체크 및 에러 횟수 카운트 -> 5번연속이면 다시 5번째로 다시 돌아가고 다시 0 -> 휴면기 돌입 1시간.
+            
+            if(!await this.check_BattleType_Solo(instanceData)) continue  // battleType === BATTLETYPE_SOLO 체크
+            
+            if(!await this.check_Exist_RouteId(instanceData)) continue  //  routeId 체크
+    
+            if(!await this.check_Route_Data(instanceData)) continue 
 
-       await this.insert_Route(); // route, characterNum, seson 조회 후 있으면, enemy 업데이트 없으면, 새로 추가
+           await this.insert_Route(instanceData); // route, characterNum, seson 조회 후 있으면, enemy 업데이트 없으면, 새로 추가
+
+        } while(instanceData.too_Many_Request)
+    
 
   }
 
@@ -63,7 +74,7 @@ export class WorkerService {
     }
         return true;
   }
-   check_Data_Exist(): Promise<boolean>{
+   check_Data_Exist(instanceData:InstanceData): Promise<boolean>{
     const thisInstance = this; 
     return  new Promise(resolve=>{
         request.get(
@@ -76,18 +87,20 @@ export class WorkerService {
             }, 
             function (error, response, body) {
                 try {
-                    WorkerService.gameJson = JSON.parse(body);
+                    instanceData.gameJson = JSON.parse(body);
                 } catch (error) {
                     resolve(false);
                 }
                 
-                if(WorkerService.gameJson?.message === "Too Many Requests"){
+                if(instanceData.gameJson?.message === "Too Many Requests"){
                     // 다시 요청하기
-                    thisInstance.set_game_ID(WorkerService.gameId - 1);
+                    instanceData.too_Many_Request = true;
                     resolve(false);
                 }
                 
-                if(WorkerService.gameJson?.code===200) {
+                instanceData.too_Many_Request = false;
+
+                if(instanceData.gameJson?.code===200) {
                     // 성공
                     WorkerService.errorCount = 0;
                     resolve(true)
@@ -99,25 +112,27 @@ export class WorkerService {
     });
     }   
 
-    check_Route_Data(): Promise<boolean>{
+    check_Route_Data(instanceData: InstanceData): Promise<boolean>{
     const thisInstance = this; 
       return new Promise(resolve=>{
          request.get(
             {
-                uri: process.env.ETERNAL_API_ROUTE + WorkerService.routeId,
+                uri: process.env.ETERNAL_API_ROUTE + instanceData.routeId,
                 headers:{
                     "x-api-key": process.env.ETERNAL_API_KEY,
                     "accept": "application/json",
                 },
             }, 
             function (error, response, body) {
-                WorkerService.routeJson = JSON.parse(body);
+                instanceData.routeJson = JSON.parse(body);
 
-                if(WorkerService.routeJson?.message === "Too Many Requests"){
-                    thisInstance.set_game_ID(WorkerService.gameId - 1);
+                if(instanceData.routeJson?.message === "Too Many Requests"){
+                    console.log("Too Many Requests");
+                    instanceData.too_Many_Request = true;
                     resolve(false);
                 }
-                if(WorkerService.routeJson?.code===200) {
+                instanceData.too_Many_Request = false;
+                if(instanceData.routeJson?.code===200) {
                     resolve(true)
                 }else{
                     resolve(false);
@@ -126,40 +141,44 @@ export class WorkerService {
     });
 
     }   
-  check_BattleType_Solo(){      
-    return WorkerService.gameJson?.userGames[0].matchingTeamMode === BATTLE_TYPE_SOLO
+  check_BattleType_Solo(instanceData:InstanceData){ 
+      if(!instanceData.gameJson) {
+          console.log("WorkerService.gameJson: ",instanceData.gameJson);
+          console.log("WorkerService.gameJson?.userGames[0].matchingTeamMode === BATTLE_TYPE_SOLO", instanceData.gameJson?.userGames[0].matchingTeamMode === BATTLE_TYPE_SOLO);
+      }
+    return instanceData.gameJson?.userGames[0].matchingTeamMode === BATTLE_TYPE_SOLO
   }
-  async find_Rank_1st(){
-    WorkerService.rank_1st_Json = await WorkerService.gameJson?.userGames.find(element => element.gameRank === 1);
-    return WorkerService.rank_1st_Json
+  async find_Rank_1st(instanceData: InstanceData){
+    instanceData.rank_1st_Json = await instanceData.gameJson?.userGames.find(element => element.gameRank === 1);
+    return instanceData.rank_1st_Json
   }
-  async get_InGame_Character_List() {
-    const characterList = await WorkerService.gameJson?.userGames.filter(element => element.gameRank !== 1).map(element => +element.characterNum);
+  async get_InGame_Character_List(instanceData: InstanceData) {
+    const characterList = await instanceData.gameJson?.userGames.filter(element => element.gameRank !== 1).map(element => +element.characterNum);
     const result = new Array(100);
     result.fill(0);
 
     await characterList.forEach(element => {
         result[element] = result[element] + 1
     });
-    WorkerService.inGame_Character_list = result;
-    return WorkerService.inGame_Character_list
+    instanceData.inGame_Character_list = result;
+    return instanceData.inGame_Character_list
   }
-  async check_Exist_RouteId(){
-    WorkerService.routeId = (await this.find_Rank_1st())?.routeIdOfStart;
-    if(!WorkerService.routeId){
+  async check_Exist_RouteId(instanceData: InstanceData){
+    instanceData.routeId = (await this.find_Rank_1st(instanceData))?.routeIdOfStart;
+    if(!instanceData.routeId){
         return false
-    }else if(WorkerService.routeId < 1){
+    }else if(instanceData.routeId < 1){
         return false
     }else{
         return true
     }
   }
 
-  async insert_Route(){
+  async insert_Route(instanceData: InstanceData){
 
-    await this.get_InGame_Character_List();
+    await this.get_InGame_Character_List(instanceData);
 
-    const insertDTO = this.make_Route_DTO();
+    const insertDTO = this.make_Route_DTO(instanceData);
 
     const route = await this.routeRepository.findOne({
         route: insertDTO.route,
@@ -179,17 +198,18 @@ export class WorkerService {
     }
   }
 
-  make_Route_DTO(){
+  make_Route_DTO(instanceData: InstanceData){
     
     const insertRoute = new Route();
-    insertRoute.route = WorkerService.routeJson?.result?.recommendWeaponRoute?.paths;
-    insertRoute.character = WorkerService.rank_1st_Json?.characterNum;
-    insertRoute.season = WorkerService.rank_1st_Json?.seasonId;
-    insertRoute.routeId = WorkerService.rank_1st_Json?.routeIdOfStart;
-    insertRoute.enemy_character_count = WorkerService.inGame_Character_list;
+    insertRoute.route = instanceData.routeJson?.result?.recommendWeaponRoute?.paths;
+    insertRoute.character = instanceData.rank_1st_Json?.characterNum;
+    insertRoute.season = instanceData.rank_1st_Json?.seasonId;
+    insertRoute.routeId = instanceData.rank_1st_Json?.routeIdOfStart;
+    insertRoute.enemy_character_count = instanceData.inGame_Character_list;
     insertRoute.count = 1;
-    insertRoute.likeScore = WorkerService.routeJson?.result?.recommendWeaponRoute?.likeScore;    
-    console.log(insertRoute);
+    insertRoute.likeScore = instanceData.routeJson?.result?.recommendWeaponRoute?.likeScore;    
+    // console.log(WorkerService.gameId);
+    // console.log(insertRoute);
     return insertRoute;
   }
 
